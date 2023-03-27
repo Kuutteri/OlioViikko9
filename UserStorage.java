61044eed6efc6ddc8f2c253e3bad8736e69d12bd
Kuutteri <128422877+Kuutteri@users.noreply.github.com>
2023-03-27 18:34:40 +0300

    Create UserStorage.java

---
+++ UserStorage.java
@@ -0,0 +1,34 @@
+package com.example.user;
+
+import java.util.ArrayList;
+
+public class UserStorage {
+    private ArrayList<User> users = new ArrayList<>();
+
+    private static UserStorage userStorage = null;
+
+    private UserStorage() {
+    }
+
+    public static UserStorage getInstance() {
+        if (userStorage == null) {
+            userStorage = new UserStorage();
+        }
+        return userStorage;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void listUsers() {
+        for (User user: users) {
+            System.out.println(user.getFirstName() + " " + user.getLastName());
+        }
+    }
+
+}
