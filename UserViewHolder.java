5e4246784a95a2ccff784ab4b6f0e0c49f54434f
Kuutteri <128422877+Kuutteri@users.noreply.github.com>
2023-03-27 18:35:13 +0300

    Create UserViewHolder.java

---
+++ UserViewHolder.java
@@ -0,0 +1,20 @@
+package com.example.user;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class UserViewHolder extends RecyclerView.ViewHolder {
+    ImageView userImage;
+    TextView userName, userEmail, userDegree;
+    public UserViewHolder(@NonNull View itemView) {
+        super(itemView);
+        userImage = itemView.findViewById(R.id.ivUserImage);
+        userName = itemView.findViewById(R.id.txtUserName);
+        userEmail = itemView.findViewById(R.id.txtUserEmail);
+        userDegree = itemView.findViewById(R.id.txtUserDegree);
+    }
+}
